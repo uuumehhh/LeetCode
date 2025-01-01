72fe1a26102b4bbc45f6bd33fedf6c317805311e
Yumeh <167229712+uuumehhh@users.noreply.github.com>
2025-01-02 00:01:15 +0900

    Create Max_Consecutive_Ones.py

---
+++ Max_Consecutive_Ones.py
@@ -0,0 +1,16 @@
+# 0,1로 이루어진 배열 입력 / 연속된 1의 최대 개수 출력
+
+count = 0
+max_count = 0
+
+num = list(map(int, input().split(',')))
+
+for i in range(len(num)):
+    if num[i] == 1:
+        count += 1
+    else:
+        max_count = max(max_count, count)
+        count = 0
+
+max_count = max(max_count, count)
+print(max_count)
